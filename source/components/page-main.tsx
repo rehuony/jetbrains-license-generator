c6d0d9e598f1a7b170da021914e896bbc7a72110
reagin <reagin@163.com>
2025-10-10 11:15:38 +0800

    feat: initial implementation product card

--- source/components/page-main.tsx
+++ source/components/page-main.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/shadcn/button';
 import { useJsonData } from '@/hooks/use-json-data';
 
 export function PageMain() {
@@ -19,25 +18,31 @@ export function PageMain() {
   const ideList = data.ides;
 
   return (
-    <main className="relative flex flex-wrap items-center justify-between p-4 gap-12">
+    <main className="grid grid-cols-[repeat(auto-fill,_minmax(min(20rem,_100%),_1fr))] justify-items-center content-center px-6 py-10 gap-14">
       {ideList.map((item) => {
+        const handleClick = () => {
+          console.log(`clicked ${item.name}`);
+        };
+
         return (
-          <article key={item.code} className="rounded-lg shadow-xl bg-card-foreground/5 shadow-card-foreground/20 select-none w-90">
-            <header className="flex items-center justify-between px-6 py-2 pb-0 gap-32 border-b-1">
-              <div className="size-16 translate-y-1/2">
-                <img src={item.icon} alt={`${item.name}'s logo`} className="size-full" />
-              </div>
-              <div className="px-8 py-2 text-sm text-card-foreground/50 rounded-full">
-                <a href={item.link} target="_blank" rel="noopener noreferrer">website</a>
-              </div>
+          <article key={item.code} className="w-5/6 rounded-lg shadow-xl bg-card-foreground/5 shadow-card-foreground/20 select-none">
+            <header className="flex items-center justify-between px-4 py-2 pb-0 border-b-1">
+              <span className="size-16 translate-y-1/2">
+                <img src={item.icon} alt={`${item.name}'s logo`} className="size-full no-drag" />
+              </span>
+              <span className="text-sm text-card-foreground/50 rounded-full border cursor-pointer hover:text-card-foreground/80 hover:border-ring">
+                <a href={item.link} target="_blank" rel="noopener noreferrer" className="block px-8 py-2 ">
+                  website
+                </a>
+              </span>
             </header>
-            <section className="flex flex-col gap-8 px-4 pt-8 pb-4 container">
+            <section className="flex flex-col gap-8 px-4 pt-8 pb-4">
               <span className="text-2xl font-mono font-light truncate translate-y-1/2">
                 {item.name}
               </span>
-              <Button className="w-full text-wrap break-all wrap-anywhere">
-                {'*'.repeat(128)}
-              </Button>
+              <span onClick={handleClick} className="relative p-2 text-left text-sm text-card-foreground/50 hover:text-transparent text-wrap wrap-anywhere after:absolute after:flex after:items-center after:justify-center after:left-0 after:top-0 after:size-full after:rounded-full after:text-card-foreground/80 after:font-light hover:after:bg-card-foreground/10 hover:after:content-['Copy_to_clipboard']">
+                {'*'.repeat(88)}
+              </span>
             </section>
           </article>
         );
